b21cd9e2c04f2c222040f2b3c3dc6ecbb82a6531
danenrich <dan@enrichconsulting.com>
2013-05-07 16:49:43 -0700

    Problem 2 revision with lower but before remove punctuation.

--- littlefile.py
+++ littlefile.py
@@ -1,10 +1,8 @@
 
+
 with open("output.txt") as myfile:
     head = [myfile.next() for x in xrange(20)]
     text_file = open("problem_1.txt", "w")
-    text_file.write(str(head))
+    text_file.write(str(head)+"\n")
     text_file.close()
 myfile.close
-
-
-print head
--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,11 +1,15 @@
 import sys
 import json
+import regex as re
 
 def hw():
     print 'Hello, world!'
 
 def lines(fp):
     print str(len(fp.readlines()))
+
+def remove_punctuation(text):
+    return re.sub(ur"\p{P}+", "", text)
 
 def main():
     sent_file = open(sys.argv[1])   
@@ -17,19 +21,16 @@ def main():
     for line in sent_file:
         term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
         scores[term] = int(score)  # Convert the score to an integer.
-        """ May need to do upper or lowercase comparison
-        newscores = {} # initialize an empty dictionary
-        newscores = scores((k.upper(), v) for k,v in scores.iteritems())
-        """
+
     #print scores.items() # Print every (term, score) pair in the dictionary         
     
     results = json.load(tweet_file) #['results']  #this is a dictionary
     tweets = results['results'] #this is a list 
     for onetweet in tweets:
-        textdata = onetweet['text']
+        textdata = onetweet['text'].lower() #.upper() **sentiment file is all lower case
         totalscore = 0
         for word in textdata.split():
-            cleanword = word.encode('utf-8') #.upper() **not doing case comparison
+            cleanword = word.encode('utf-8') 
             # strung = repr(cleanword) don't want to send as string  
             #Note: may need to remove punctuation
             #print cleanword
