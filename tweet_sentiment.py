848e7076d2898364b0a74193c0f6faa61a63c3c8
danenrich <dan@enrichconsulting.com>
2013-05-07 11:14:26 -0700

    Use course logic for creating sentiment dictionary.

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -14,34 +14,16 @@ def main():
     tweet_file = open(sys.argv[2])
     #sflines = lines(sent_file)
     #twlines = lines(tweet_file)
-    """
-    for n in sent_file:
-        results = json.loads(n)
-    """
-    """
-    with sent_file as file_object:
-    # skip the first two lines
-        results = list(csv.DictReader(file_object, dialect='excel-tab'))
-    """
-    with sent_file as infile:
-        reader = csv.reader(infile)
-        
-    text_file = open("sent_out.txt", "w")
-    with text_file as outfile:
-        #writer = csv.writer(outfile)
-        mydict = {rows[0]:rows[1] for rows in reader}
-        for n in mydict:
-            text_file.write("%s\n" % n)
-    text_file.close()
-     
-"""    
-    text_file = open("sent_out.txt", "w")
-    for t in results:
-        text_file.write("%s\n" % t)
-        #line = str(repr(t['text']))
-        #text_file.write("%s\n" % line)
-"""
     
+    afinnfile = open("AFINN-111.txt")
+    scores = {} # initialize an empty dictionary
+    for line in afinnfile:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  # Convert the score to an integer.
+    
+    print scores.items() # Print every (term, score) pair in the dictionary     
+        
+        
 
 if __name__ == '__main__':
     main()
