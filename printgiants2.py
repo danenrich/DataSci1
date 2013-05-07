ce484612dbc3f6dd94512afbca5cc96f3104ff35
danenrich <dan@enrichconsulting.com>
2013-05-07 14:23:56 -0700

    Problem 2: Transformed words into numbers.

--- printgiants2.py
+++ printgiants2.py
@@ -18,14 +18,14 @@ import json
 #response = urllib.urlopen("http://search.twitter.com/search.json?q=microsoft")
 
 
-qstring = "http://search.twitter.com/search.json?q=@hankschulman&count=20"
+qstring = "http://search.twitter.com/search.json?q=@hankschulman&page=1"
 response = urllib.urlopen(qstring)
 jsondata = json.load(response)
 #print jsondata
 results = jsondata #not using just the "results"; assuming same as live stream
 text_file = open("giants_out.txt", "w")
 for t in results:
-    text_file.write(str(repr(results)))
+    text_file.write(str(results))
     """
     line = str(repr(t['text']))
     line = str(repr(t))
--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -10,7 +10,7 @@ def lines(fp):
     print str(len(fp.readlines()))
 
 def main():
-    sent_file = open(sys.argv[1])
+    sent_file = open(sys.argv[1])   
     tweet_file = open(sys.argv[2])
     #sflines = lines(sent_file)
     #twlines = lines(tweet_file)
@@ -19,26 +19,36 @@ def main():
     for line in sent_file:
         term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
         scores[term] = int(score)  # Convert the score to an integer.
-    
+        """ May need to do upper or lowercase comparison
+        newscores = {} # initialize an empty dictionary
+        newscores = scores((k.upper(), v) for k,v in scores.iteritems())
+        """
     #print scores.items() # Print every (term, score) pair in the dictionary     
     
-    tweets = []
-    clean_file = tweet_file #json.dumps(tweet_file)
-    for line in clean_file:
-        line = line.encode('utf-8')
-        print line
-        #tweets.append(json.loads(line))
-
-    tweet_json = json.load(tweet_file)
- #   results = tweet_json['results']
-#  print results
-    """
-    for t in results:
-        print repr(t['text']),"\n"
-    """
+    tweettext = {}
+    results = json.load(tweet_file) #['results']
+    tweets = results['results']
+    data = tweets[1]
+    textdata = data['text']
+    for word in textdata.split():
+        cleanword = word.encode('utf-8') #.upper() **not doing case comparison
+        # strung = repr(cleanword) don't want to send as string  
+        #Note: may need to remove punctuation
+        print cleanword
+        wordscore = scores.get(cleanword)
+        if wordscore == None:
+            wordscore = 0
+        print wordscore
+        """
+        try:
+            print scores.get(strung)
+        except KeyError:
+            print 
+        """
     
     sent_file.close
     tweet_file.close
 
 if __name__ == '__main__':
     main()
+
