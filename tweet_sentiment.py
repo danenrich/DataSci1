8307da7ad6a83cebdb96703a0d0b01601bfd3a58
danenrich <dan@enrichconsulting.com>
2013-05-07 15:50:28 -0700

    Whoops--problem 2 output should just be a number.

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -39,7 +39,7 @@ def main():
             if wordscore == None:
                 wordscore = 0
             totalscore = totalscore + wordscore
-        sys.stdout.write("sentiment:" + str(totalscore)+"\n")
+        sys.stdout.write(str(totalscore)+"\n")
 
     sent_file.close
     tweet_file.close
