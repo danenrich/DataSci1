07e473659a765556040f55a624e3202a6c87ad35
danenrich <dan@enrichconsulting.com>
2013-05-09 22:10:45 -0700

    Problem 6: re-worked to use entities dictionary.

--- top_ten.py
+++ top_ten.py
@@ -9,23 +9,20 @@ def main():
     with tweet_file as f:  #when this is open('output.txt') it works 5/8/13
         for x in f:
             tweets.append(json.loads(x)) 
-
-    textdata = [] #This is the final list of new terms
-    for onetweet in tweets: 
-    #for onetweet in tweets[:2000]: #REMOVE THIS XRANGE CONSTRAINT
-        if onetweet.has_key('text') == 1:
-            textual = onetweet['text']
-            #cleantext = textual.encode('utf-8')
-            #cleanertext = cleantext.rstrip('?:!.,;') #Removing punctuation
-            #cleanertext = cleanertext.replace("\n"," ")
-            cleanertext = textual #.replace("\n"," ")
-            textdata = textdata + [cleanertext] #list of text strings
-
-    hashlist = []
+    entities = [] #This is the final list of new terms
     hashtemp = []
-    for hashy in textdata:
-        hashtemp = list(set(part for part in hashy.split() if part.startswith('#')))
-        hashlist = hashlist + hashtemp
+    hashlist = []
+    #for onetweet in tweets: 
+    for onetweet in tweets: #REMOVE THIS XRANGE CONSTRAINT
+        if onetweet.has_key('entities') == 1:
+            entities = onetweet['entities']
+            if entities.has_key('hashtags')==1:
+                if entities["hashtags"] != None:
+                    hashmash = entities["hashtags"]
+                    for hashtemp in hashmash:
+                        #print hashtemp["text"]
+                        hashlist = hashlist + [hashtemp["text"]]
+
     #print hashlist
     freqtable = collections.Counter(hashlist)
     sorttable = sorted(freqtable.items(), key=lambda item: item[1], reverse=True)
