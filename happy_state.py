fd8626b1a8c6ffee72a8fb08515202f9f85bbdb9
danenrich <dan@enrichconsulting.com>
2013-05-09 17:36:19 -0700

    First pass at problem 5

--- happy_state.py
+++ happy_state.py
@@ -12,29 +12,36 @@ def main():
 
     #print scores.items() # Print every (term, score) pair in the dictionary         
  
-    data = []
+    tweets = []
     with tweet_file as f:  #when this is open('output.txt') it works 5/8/13
         for x in f:
-            data.append(json.loads(x))
-
-    tweets = data #['results'] #this is a list 
-
+            tweets.append(json.loads(x))
+    
+    highscore = -10000000
+    
     for onetweet in tweets:
         if onetweet.has_key('text') == 1 and onetweet.has_key('place') == 1:
             placetweet = onetweet["place"]
             if placetweet != None:
                 if placetweet.has_key('country') and placetweet.has_key('full_name'):
-                    if placetweet['country_code']=='MX':
-                        textdata = onetweet['text'] # **sentiment file is all lower case
-                        totalscore = 0
-                        for word in textdata.split():
-                            cleanword = word.encode('utf-8')
-                            cleanerword = cleanword.rstrip('?:!.,;') #Removing punctuation
-                            wordscore = scores.get(cleanerword)
-                            if wordscore == None:
-                                wordscore = 0
-                            totalscore = totalscore + wordscore
-        sys.stdout.write(str(totalscore)+"\n")
+                    countrycode = placetweet['country_code']
+                    if countrycode=='US':
+                        fullname = placetweet['full_name']
+                        if len(fullname) > 1:
+                            textdata = onetweet['text'] # **sentiment file is all lower case
+                            totalscore = 0
+                            for word in textdata.split():
+                                cleanword = word.encode('utf-8')
+                                cleanerword = cleanword.rstrip('?:!.,;') #Removing punctuation
+                                wordscore = scores.get(cleanerword)
+                                if wordscore == None:
+                                    wordscore = 0
+                                totalscore = totalscore + wordscore
+                                if totalscore > highscore:
+                                    highscore = totalscore
+                                    happystate = fullname[-2:]
+    
+    sys.stdout.write(str(happystate))
 
     sent_file.close
     tweet_file.close
