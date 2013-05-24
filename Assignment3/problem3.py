ac616975300d0dde6bc3197c64f84c40a1e3cc8a
danenrich <dan@enrichconsulting.com>
2013-05-23 21:33:26 -0700

    Problem 3. I think it's solved but it's yelling at me about tuples.

---
+++ Assignment3/problem3.py
@@ -0,0 +1,34 @@
+import MapReduce
+import sys
+
+"""
+Word Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    key = record[0]
+    friend = record[1]
+    mr.emit_intermediate(key, friend)
+
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of occurrence counts
+    output = []
+    numfriends = len(list_of_values)
+    output = [key] + [numfriends] 
+    mr.emit(output)
+          
+    #mr.emit((key, cleanlist))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
