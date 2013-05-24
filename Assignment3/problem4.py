41f4c4550bfe7a0c3197acc52803fb78c6596a1e
danenrich <dan@enrichconsulting.com>
2013-05-23 23:57:33 -0700

    Don't want to lose this.

--- Assignment3/problem4.py
+++ Assignment3/problem4.py
@@ -13,27 +13,17 @@ mr = MapReduce.MapReduce()
 def mapper(record):
     # key: document identifier
     # value: document contents
-    dict1 = []
-    key = record[0]
-    friend = record[1]
-    #tupper = sort() #If you sort the tuple... 
+ 
     tupper = tuple(record)
-    #tuppy = tuple(reversed(record))
     mr.emit_intermediate(tupper,record)
     mr.emit_intermediate(tuple(reversed(record)),tupper)
-    #mr.emit_intermediate(tuppy,tupper)
-    #mr.emit_intermediate(friend,record)
-    
-    #mr.emit_intermediate(key,mr.emit_intermediate(key, friend))
 
 def reducer(key, list_of_values):
     # key: word
     # value: list of occurrence counts
-    goset = {}
-    #goset = map(tuple,list_of_values)
-    #goset = [list(x) for x in set(tuple(x) for x in list_of_values)]
-    #[i for i in set(a) if a.count(i)<2]
-    mr.emit((key,list_of_values))
+    newlist = []
+    newlist = [s for s in list_of_values if len(s) == 2] 
+    mr.emit(newlist)
                     
 # Do not modify below this line
 # =============================
