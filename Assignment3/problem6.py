3f696ec83b9799ac2faad38e392e923505cddcab
danenrich <dan@enrichconsulting.com>
2013-05-24 12:27:07 -0700

    Problem 6: Right answer, now need to format.

--- Assignment3/problem6.py
+++ Assignment3/problem6.py
@@ -34,7 +34,7 @@ def mapper(record):
             mr.emit_intermediate(key, vallist)
 
     if matrixid == "b":
-    #a:every val in a gets multiplied in each col of b. row in a is its row in c. for a, we need to know its original column and its value. need to gen keys for each col of c.
+    #a:col in b is its col in c. for b, we need to know its original row and its value. need to gen keys for each row of c.
         for i in range(0,maxkey):
             key = (i,colnum)
             vallist = [matrixid,rownum,val]
@@ -44,8 +44,22 @@ def reducer(key, list_of_values):
     # key: word
     # value: list of occurrence counts
     
-    #numfriends =  len(list_of_values)
-    mr.emit((key,list_of_values))
+    #can just iterate through the "a"s. if there is no corresponding b (or an a is missing), it's zero anyway.
+    celltotal=0
+    for a in list_of_values:
+        if a[0]=="a":
+            aindex = a[1]
+            aval = a[2]
+            for b in list_of_values:
+                bindex = b[1]
+                bval = b[2]
+                if b[0]=="b" and bindex==aindex:
+                    cellmult = bval * aval
+                    #print cellmult
+                    celltotal=+cellmult
+    mr.emit((key,celltotal))
+    #print key, list_of_values
+    
                     
 # Do not modify below this line
 # =============================
