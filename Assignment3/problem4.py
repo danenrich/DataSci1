84de9484eb75e37a3e3f6a6153bf7e891736a30d
danenrich <dan@enrichconsulting.com>
2013-05-23 23:49:57 -0700

    Problem 4. Maybe I have the right map now. Too long.

--- Assignment3/problem4.py
+++ Assignment3/problem4.py
@@ -16,17 +16,23 @@ def mapper(record):
     dict1 = []
     key = record[0]
     friend = record[1]
-    #tupper = tuple(reversed(record))
-    mr.emit_intermediate(key,record)
-    mr.emit_intermediate(friend,record)
+    #tupper = sort() #If you sort the tuple... 
+    tupper = tuple(record)
+    #tuppy = tuple(reversed(record))
+    mr.emit_intermediate(tupper,record)
+    mr.emit_intermediate(tuple(reversed(record)),tupper)
+    #mr.emit_intermediate(tuppy,tupper)
+    #mr.emit_intermediate(friend,record)
     
     #mr.emit_intermediate(key,mr.emit_intermediate(key, friend))
 
 def reducer(key, list_of_values):
     # key: word
     # value: list of occurrence counts
-    #print key, list_of_values
-    #blah = 1
+    goset = {}
+    #goset = map(tuple,list_of_values)
+    #goset = [list(x) for x in set(tuple(x) for x in list_of_values)]
+    #[i for i in set(a) if a.count(i)<2]
     mr.emit((key,list_of_values))
                     
 # Do not modify below this line
